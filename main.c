f3228d4e0a0282aed8bd0ec7daa4c3412ee19cef
Lukazx15 <nattananpunpetch@gmail.com>
2025-09-17 01:28:50 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <conio.h>
 
+struct Contact {
+    char company[50];
+    char person[50];
+    char phone[50];
+    char email[50];
+} c;
+
+FILE *fp;
+
+void addContact();
+
 int main() {
-    int chioce;
+    int choice;
 
     while (1) {
         system("cls");
@@ -15,15 +27,16 @@ int main() {
         printf("5. Update Contact\n");
         printf("0. Exit\n\n");
         printf("Enter your choice: ");
-        scanf("%d", &chioce);
+        scanf("%d", &choice);
+        getchar();
 
-        switch (chioce) {
+        switch (choice) {
         case 0:
             printf("Exiting program\n");
             exit(0);
 
         case 1:
-            printf(">> Add Contact selected\n");
+            addContact();
             break;
 
         case 2:
@@ -46,9 +59,39 @@ int main() {
             printf("Invalid Choice\n");
         }
 
-        printf("Press Any Key To Continue...");
+        printf("Press Any Key To Continue:");
         getch();
     }
 
     return 0;
+}
+
+void addContact() {
+    fp = fopen("contacts.csv", "a");
+    if (!fp) {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    printf("Enter company name: ");
+    fgets(c.company, sizeof(c.company), stdin);
+    c.company[strcspn(c.company, "\n")] = 0;
+
+    printf("Enter contact person name: ");
+    fgets(c.person, sizeof(c.person), stdin);
+    c.person[strcspn(c.person, "\n")] = 0;
+
+    printf("Enter phone number: ");
+    fgets(c.phone, sizeof(c.phone), stdin);
+    c.phone[strcspn(c.phone, "\n")] = 0;
+
+    printf("Enter email: ");
+    fgets(c.email, sizeof(c.email), stdin);
+    c.email[strcspn(c.email, "\n")] = 0;
+
+    
+    fprintf(fp, "%s,%s,%s,%s\n", c.company, c.person, c.phone, c.email);
+    fclose(fp);
+
+    printf("Contact Added Successfully\n");
 }
